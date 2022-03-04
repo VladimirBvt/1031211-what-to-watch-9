6b5553ea07053a4b9cab98ac29bc3712a7c41f5d
Vladimir <vladimir@MacBook-Pro-Vladimir.local>
2022-03-04 20:08:18 +0300

    Исправляет ошибку Линтера.

--- project/src/components/sign-in/sign-in.tsx
+++ project/src/components/sign-in/sign-in.tsx
@@ -1,42 +1,42 @@
-import SvgHidden from "../svg-hidden/svg-hidden";
-import Footer from "../footer/footer";
-import Header from "../header/header";
+import SvgHidden from '../svg-hidden/svg-hidden';
+import Footer from '../footer/footer';
+import Header from '../header/header';
 
 function SignIn () {
   return (
     <>
       <SvgHidden set='' />
 
-      <div className="user-page">
+      <div className='user-page'>
 
         <Header set='forSignIn' />
 
-        {/*<header className="page-header user-page__head">
-          <div className="logo">
-            <a href="#" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
+        {/*<header className='page-header user-page__head'>
+          <div className='logo'>
+            <a href='#' className='logo__link'>
+              <span className='logo__letter logo__letter--1'>W</span>
+              <span className='logo__letter logo__letter--2'>T</span>
+              <span className='logo__letter logo__letter--3'>W</span>
             </a>
           </div>
 
-          <h1 className="page-title user-page__title">Sign in</h1>
+          <h1 className='page-title user-page__title'>Sign in</h1>
         </header>*/}
 
-        <div className="sign-in user-page__content">
-          <form action="#" className="sign-in__form">
-            <div className="sign-in__fields">
-              <div className="sign-in__field">
-                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email"/>
-                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+        <div className='sign-in user-page__content'>
+          <form action='#' className='sign-in__form'>
+            <div className='sign-in__fields'>
+              <div className='sign-in__field'>
+                <input className='sign-in__input' type='email' placeholder='Email address' name='user-email' id='user-email'/>
+                <label className='sign-in__label visually-hidden' htmlFor='user-email'>Email address</label>
               </div>
-              <div className="sign-in__field">
-                <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password"/>
-                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+              <div className='sign-in__field'>
+                <input className='sign-in__input' type='password' placeholder='Password' name='user-password' id='user-password'/>
+                <label className='sign-in__label visually-hidden' htmlFor='user-password'>Password</label>
               </div>
             </div>
-            <div className="sign-in__submit">
-              <button className="sign-in__btn" type="submit">Sign in</button>
+            <div className='sign-in__submit'>
+              <button className='sign-in__btn' type='submit'>Sign in</button>
             </div>
           </form>
         </div>
