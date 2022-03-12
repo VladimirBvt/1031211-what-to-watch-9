84e2675bd8ffe846634bfd5cc17e8eee8bb6c0d8
Vladimir <vladimir@MacBook-Pro-Vladimir.local>
2022-03-12 18:45:52 +0300

    Создает мокковые данные фильмов и комментарии к ним.

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,39 @@
+type Reviews = {
+  id: number,
+  review: string,
+}[];
+
+export const reviews: Reviews = [
+  {
+    id: 0,
+    review: '0uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 1,
+    review: '1uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 2,
+    review: '2uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 3,
+    review: '3uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 4,
+    review: '4uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 5,
+    review: '5uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 6,
+    review: '6uujjiiiedkkdooeqqweed',
+  },
+  {
+    id: 7,
+    review: '7uujjiiiedkkdooeqqweed',
+  },
+];
